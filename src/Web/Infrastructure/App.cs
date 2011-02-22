8e73a4af6cdd740a7d04bb1d92655c8b03f0ca04
Mogens Heller Grabe <mookid8000@gmail.com>
2011-02-22 21:13:47 +0100

    hacked the Google Maps API script tag in place

---
+++ src/Web/Infrastructure/App.cs
@@ -0,0 +1,18 @@
+﻿namespace Web.Infrastructure
+{
+    public class App
+    {
+        static readonly AppEnvironmentHelperFromAppSettings Helper = new AppEnvironmentHelperFromAppSettings();
+        static readonly AppEnvironment AppEnvironment;
+
+        static App()
+        {
+            AppEnvironment = Helper.Current;
+        }
+
+        public static AppEnvironment Environment
+        {
+            get { return AppEnvironment; }
+        }
+    }
+}
